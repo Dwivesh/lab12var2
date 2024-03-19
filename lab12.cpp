7ef32544c0118aea4c5f0b729f6d4e7031e15c46
Евдокия Фофанова <87367250+Dwivesh@users.noreply.github.com>
2024-03-19 19:34:41 +0300

    lab12v2

---
+++ lab12.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+class RangeIterator {
+private:
+    typename vector<T>::iterator current;
+    typename vector<T>::iterator end;
+
+public:
+    RangeIterator(typename vector<T>::iterator begin, typename vector<T>::iterator end)
+        : current(begin), end(end) {}
+
+    RangeIterator& operator++() {
+        ++current;
+        return *this;
+    }
+
+    RangeIterator& operator--() {
+        --current;
+        return *this;
+    }
+
+    T& operator*() {
+        return *current;
+    }
+
+    bool operator!=(const RangeIterator& other) const {
+        return current != other.current;
+    }
+};
+
+int main() {
+    vector<int> vec = {1, 2, 3, 4, 5};
+
+    RangeIterator<int> begin(vec.begin(), vec.end());
+    RangeIterator<int> end(vec.end(), vec.end());
+
+    for (RangeIterator<int> it = begin; it != end; ++it) {
+        cout << *it << " ";
+    }
+
+    return 0;
+}
